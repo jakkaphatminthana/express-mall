ab493dbcfbaef71717ef0e5308056dfb215ec820
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-07-31 14:50:08 +0700

    chore: setup database with docker-compose

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 const config = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+
+  //Database
+  DB_NAME: process.env.POSTGRES_DB,
+  DB_USER: process.env.POSTGRES_USER,
+  DB_PASSWORD: process.env.POSTGRES_PASSWORD,
+  DB_PORT: parseInt(process.env.POSTGRES_PORT || '5432'),
 };
 
 export default config;
