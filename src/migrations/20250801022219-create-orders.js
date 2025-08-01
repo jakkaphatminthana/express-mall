a80062e2282e004e1d129bf191375b1011e87aa5
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-08-01 10:16:18 +0700

    feat : order :: migration, model

---
+++ src/migrations/20250801022219-create-orders.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      memberId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          mode: 'Members',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      pointsEarn: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  },
+};
---
+++ src/models/order.ts
@@ -0,0 +1,73 @@
+import { Association, DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '@/config/connection';
+
+interface OrderAttributes {
+  id: number;
+  memberId?: number;
+  pointsEarn: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// auto
+interface OrderCreationAttributes
+  extends Optional<
+    OrderAttributes,
+    'id' | 'pointsEarn' | 'createdAt' | 'updatedAt'
+  > {}
+
+class Order
+  extends Model<OrderAttributes, OrderCreationAttributes>
+  implements OrderAttributes
+{
+  public id!: number;
+  public memberId?: number;
+  public pointsEarn!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  public static associations: {
+    member: Association<Order, any>;
+    orderProducts: Association<Order, any>;
+    pointTransaction: Association<Order, any>;
+  };
+}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    memberId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Members',
+        key: 'id',
+      },
+    },
+    pointsEarn: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        min: {
+          args: [0],
+          msg: "pointsEarn can't be negative",
+        },
+      },
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Orders',
+    timestamps: true,
+    // good performance query
+    indexes: [{ fields: ['memberId'] }, { fields: ['createdAt'] }],
+  },
+);
+
+export { Order, OrderAttributes, OrderCreationAttributes };
