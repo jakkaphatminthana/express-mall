f3f0be786183f4edcf9d9f9492a58ab70f252c93
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-08-01 11:16:05 +0700

    feat : order-product :: migration, model

---
+++ src/migrations/20250801024626-create-order-products.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Order_Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Products',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Orders',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      unitPrice: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      totalPrice: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Order_Products');
+  },
+};
---
+++ src/models/orderProduct.ts
@@ -0,0 +1,111 @@
+import { Association, DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '@/config/connection';
+
+interface OrderProductAttributes {
+  id: number;
+  productId: number;
+  orderId: number;
+  amount: number;
+  unitPrice: number;
+  totalPrice: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface OrderProductCreationAttributes
+  extends Optional<
+    OrderProductAttributes,
+    'id' | 'amount' | 'unitPrice' | 'totalPrice' | 'createdAt' | 'updatedAt'
+  > {}
+
+class OrderProduct
+  extends Model<OrderProductAttributes, OrderProductCreationAttributes>
+  implements OrderProductAttributes
+{
+  public id!: number;
+  public productId!: number;
+  public orderId!: number;
+  public amount!: number;
+  public unitPrice!: number;
+  public totalPrice!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // reference
+  public static associations: {
+    order: Association<OrderProduct, any>;
+    product: Association<OrderProduct, any>;
+  };
+}
+
+OrderProduct.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    orderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Orders',
+        key: 'id',
+      },
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Products',
+        key: 'id',
+      },
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: {
+        min: {
+          args: [1],
+          msg: 'quantity must be at least 1',
+        },
+      },
+    },
+    unitPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: {
+          args: [0],
+          msg: "unitPrice can't be negative",
+        },
+      },
+    },
+    totalPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: {
+          args: [0],
+          msg: "totalPrice can't be negative",
+        },
+      },
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Order_Products',
+    timestamps: true,
+    // good performance query
+    indexes: [{ fields: ['orderId'] }, { fields: ['productId'] }],
+    hooks: {
+      beforeSave: async (orderProduct: OrderProduct) => {
+        orderProduct.totalPrice = orderProduct.amount * orderProduct.unitPrice;
+      },
+    },
+  },
+);
+
+export { OrderProduct, OrderProductAttributes, OrderProductCreationAttributes };
