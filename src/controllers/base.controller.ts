69279c489ec1d40e0480897db0dd79b63e4d0dad
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-08-04 14:47:06 +0700

    chore: fix base controller

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 
-/* 
-body: A;
-params: B;
-query: C; 
-*/
-export type RequestBodyType<A, B, C> = Request<B, any, A, C>;
+export type RequestBodyType<A, B, C> = Request & {
+  body: A;
+  params: B;
+  query: C;
+};
 
 export type ControllerBaseFunctionType<A, B, C> = (
   request: RequestBodyType<A, B, C>,
